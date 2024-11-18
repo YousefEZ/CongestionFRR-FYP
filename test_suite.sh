ae28f1b952e1fd79d45037557a4cffcb27d195ce
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-11-18 22:01:27 +0000

    :wrench: make new directory for testing the rerouting head

--- test_suite.sh
+++ test_suite.sh
@@ -2,5 +2,6 @@
 #pip install -r requirements.txt
 
 mkdir -p traces/test_no_congestion_no_rerouting
+mkdir -p traces/test_rerouting_head
 #NS_LOG="" ./ns3 run "scratch/combined-frr.cc --tcp_senders=1 --policy_threshold=50 --dir=traces/test_no_congestion_no_rerouting/ --seed=1 --bandwidth_primary=5KBps --bandwidth_access=2KBps --bandwidth_udp_access=2KBps"
 pytest . 
