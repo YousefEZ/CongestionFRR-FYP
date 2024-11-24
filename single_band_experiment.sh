411cc4ae4dda96e2745b88773dbd9b8c6a270096
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-11-24 23:40:50 +0000

    :sparkles: update shell script to work with new layout of directories

--- single_band_experiment.sh
+++ single_band_experiment.sh
@@ -5,18 +5,22 @@ one_band_experiment()
   local test_variable=$1 
   local test_value=$2 
   local policy_threshold=$3 
-  local dir="traces/$test_variable/$test_value/$policy_threshold"
+  local dir="traces/$test_variable/" 
   local seed="234729"
   local run="1"
-  mkdir -p "$dir/frr/$seed$run/1"
-  mkdir -p "$dir/frr-no-udp/$seed$run/1"
-  mkdir -p "$dir/baseline-udp/$seed$run/1"
-  mkdir -p "$dir/baseline-no-udp/$seed$run/1"
+  local frr="$dir/frr/$seed$run/$test_value/"
+  local frr_no_udp="$dir/frr-no-udp/$seed$run/$test_value/"
+  local baseline_udp="$dir/baseline-udp/$seed$run/$test_value/"
+  local baseline_no_udp="$dir/baseline-no-udp/$seed$run/$test_value/"
+  mkdir -p $frr 
+  mkdir -p $frr_no_udp
+  mkdir -p $baseline_no_udp
+  mkdir -p $baseline_udp
 
-  NS_LOG="FRRQueue=info|prefix_time" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/frr/$seed$run/1/ --seed=$seed --run=$run --enable-rerouting --enable-udp" 2> $dir/frr/$seed$run/1/debug.log
-  NS_LOG="FRRQueue=info|prefix_time" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/frr-no-udp/$seed$run/1/ --seed=$seed --run=$run --enable-rerouting" 2> $dir/frr/$seed$run/1/debug.log
-  NS_LOG="" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/baseline-udp/$seed$run/1/ --seed=$seed --run=$run --enable-udp" 
-  NS_LOG="" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/baseline-no-udp/$seed$run/1/ --seed=$seed --run=$run"
+  NS_LOG="FRRQueue=info|prefix_time" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$frr --seed=$seed --run=$run --enable-rerouting --enable-udp" 2> $frr/debug.log
+  NS_LOG="FRRQueue=info|prefix_time" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$frr_no_udp --seed=$seed --run=$run --enable-rerouting" 2> $frr_no_udp/debug.log
+  NS_LOG="" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$baseline_udp --seed=$seed --run=$run --enable-udp" 
+  NS_LOG="" ./ns3 run "scratch/simulation.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$baseline_no_udp --seed=$seed --run=$run"
   #NS_LOG="FRRQueue=info|prefix_time" ./ns3 run "scratch/combined-frr.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/frr/$seed/1/ --seed=$seed" 2> $dir/frr/$seed/1/debug.log
   #NS_LOG="" ./ns3 run "scratch/combined-baseline-udp.cc --$test_variable=$test_value --tcp_senders=1 --policy_threshold=$policy_threshold --dir=$dir/baseline-udp/$seed/1/ --seed=$seed"
   #NS_LOG="" ./ns3 run "scratch/combined-baseline-no-udp.cc --$test_variable=$test_value --tcp_senders=1 --dir=$dir/baseline-no-udp/$seed/1/ --seed=$seed"
