21e4e892f367fa299037d3c1c045087f046a4344
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-01 14:40:06 +0000

    :sparkles: add loss function

--- analysis/__main__.py
+++ analysis/__main__.py
@@ -4,7 +4,24 @@ import click
 from analysis import discovery, graph, scenario
 
 
-@click.command("graph")
+@click.group(name="graph")
+def _graph() -> None:
+    ...
+
+
+def generate_scenarios(
+    directory: str,
+    options: Optional[list[discovery.Options]],
+    seeds: list[discovery.Seed],
+) -> list[scenario.Scenario]:
+    if not options:
+        options = discovery.discover_options(directory)
+    if not seeds:
+        seeds = discovery.discover_seeds(directory, options[0])
+    return [scenario.Scenario(directory, option, seeds) for option in options]
+
+
+@_graph.command(name="time")
 @click.option("--directory", "-d", help="Path to the directory", required=True)
 @click.option(
     "--option",
@@ -22,39 +39,64 @@ from analysis import discovery, graph, scenario
     help="Seed to plot, if not set will discover",
     default=[],
 )
-@click.option(
-    "--only-average", "-a", is_flag=True, help="Only plot the average", default=False
-)
 @click.option("--output", "-o", help="Output file name")
-def _graph(
+def _time(
     directory: str,
-    options: list[scenario.Options],
-    seeds: list[scenario.Seed],
-    only_average: bool,
+    options: list[discovery.Options],
+    seeds: list[discovery.Seed],
     output: Optional[str],
 ) -> None:
-    if not options:
-        options = discovery.discover_options(directory)
+    scenarios = generate_scenarios(directory, options, seeds)
 
-    if not seeds:
-        seeds = discovery.discover_seeds(directory, options[0])
+    results = {
+        str(flow_completion_time.option): flow_completion_time.times.average
+        for flow_completion_time in scenarios
+    }
+    standard_deviation = {
+        str(flow_completion_time.option): flow_completion_time.times.standard_deviation
+        for flow_completion_time in scenarios
+    }
+    graph.plot(results, target=output, standard_deviation=standard_deviation)
 
-    completion_times = [
-        scenario.Scenario(directory, option, seeds) for option in options
-    ]
 
-    if only_average:
-        results = {
-            str(flow_completion_time.option): flow_completion_time.average
-            for flow_completion_time in completion_times
-        }
-        standard_deviation = {
-            str(flow_completion_time.option): flow_completion_time.standard_deviation
-            for flow_completion_time in completion_times
-        }
-        graph.plot_flow_completion_time(
-            results, target=output, standard_deviation=standard_deviation
-        )
+@_graph.command("loss")
+@click.option("--directory", "-d", help="Path to the directory", required=True)
+@click.option(
+    "--option",
+    "-o",
+    "options",
+    multiple=True,
+    help="Options to plot, if not set will discover",
+    default=[],
+)
+@click.option(
+    "--seed",
+    "-s",
+    "seeds",
+    multiple=True,
+    help="Seed to plot, if not set will discover",
+    default=[],
+)
+@click.option("--output", "-o", help="Output file name")
+def _loss(
+    directory: str,
+    options: list[discovery.Options],
+    seeds: list[discovery.Seed],
+    output: Optional[str],
+) -> None:
+    scenarios = generate_scenarios(directory, options, seeds)
+
+    results = {
+        str(flow_completion_time.option): flow_completion_time.packet_loss.average
+        for flow_completion_time in scenarios
+    }
+    standard_deviation = {
+        str(
+            flow_completion_time.option
+        ): flow_completion_time.packet_loss.standard_deviation
+        for flow_completion_time in scenarios
+    }
+    graph.plot(results, target=output, standard_deviation=standard_deviation)
 
 
 if __name__ == "__main__":
