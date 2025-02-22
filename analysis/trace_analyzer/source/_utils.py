47e225234c344f6011d288a146b4f4c347f9c14e
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2025-02-22 17:41:27 +0000

    :sparkles: add more trace analyzers

--- analysis/trace_analyzer/source/_utils.py
+++ analysis/trace_analyzer/source/_utils.py
@@ -7,6 +7,15 @@ from analysis.trace_analyzer.source.socket_state import SackedByteRange
 SMSS = 1446
 
 
+def get_sacked_segments(sacks: list[SackedByteRange]) -> list[int]:
+    sacked_segments = (
+        sacked_segment
+        for sacked_range in sacks
+        for sacked_segment in range(sacked_range.start, sacked_range.end, SMSS)
+    )
+    return list(sacked_segments)
+
+
 def calculate_dropped_packets(sacks: list[SackedByteRange]) -> list[int]:
     dropped_packets = (
         dropped_packet
