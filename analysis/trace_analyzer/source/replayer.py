fd59059892c99ac7c4417b5ebc8394ad5e3d31e9
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2025-02-22 17:41:13 +0000

    :sparkles: add pcap replayer

--- analysis/trace_analyzer/source/replayer.py
+++ analysis/trace_analyzer/source/replayer.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass, field
 
 import scapy
+from scapy.all import Raw
 import scapy.packet
 from scapy.layers.inet import IP, TCP
 
-from analysis.pcap import TCP_ACK, PcapFile
+from analysis.pcap import SMSS, TCP_ACK, PcapFile
 from analysis.trace_analyzer.source.socket_state import SocketState
 from analysis.trace_analyzer.source.packet_capture import PacketCapture
 from analysis.trace_analyzer.source._utils import (
     calculate_dropped_packets,
     get_sacked_byte_ranges,
+    get_sacked_segments,
 )
 
 DUPLICATE_ACK_THRESHOLD = 3
@@ -37,9 +39,20 @@ class TcpSourceReplayer:
         self.state.dup_ack = 0
         self.state.sack_dupacks.clear()
 
+    def _update_scoreboard(self, segment: int) -> None:
+        if segment not in self.state.scoreboard:
+            self.event_handlers.on_scoreboard_add(segment, self.state)
+            self.state.scoreboard.add(segment)
+
+    def _cumulative_ack(self, packet: scapy.packet.Packet) -> None:
+        for segment in range(self.state.last_acked_seq, packet[TCP].ack + SMSS, SMSS):
+            self._update_scoreboard(segment)
+
     def _handle_sacks(self, packet: scapy.packet.Packet) -> None:
         sacks = get_sacked_byte_ranges(packet)
         if sacks is not None:
+            for segment in get_sacked_segments(sacks):
+                self._update_scoreboard(segment)
             for dropped_packet in calculate_dropped_packets(sacks):
                 if self.state.sack_dupacks[dropped_packet] != DUPLICATE_ACK_THRESHOLD:
                     self.state.sack_dupacks[dropped_packet] += 1
@@ -55,8 +68,11 @@ class TcpSourceReplayer:
 
     def _handle_new_ack(self, packet: scapy.packet.Packet):
         self.event_handlers.on_ack(packet, self.state)
+        if packet[TCP].ack > self.state.recovery_point:
+            self._exit_recovery()
         self._clear_dup_acks()
         self.state.last_acked_seq = packet[TCP].ack
+        self._cumulative_ack(packet)
 
     def _handle_ack(self, packet: scapy.packet.Packet):
         if self._is_dup_ack(packet):
@@ -64,21 +80,54 @@ class TcpSourceReplayer:
         else:
             self._handle_new_ack(packet)
 
+        self.state.last_ack_timestamp = float(packet.time)
+
     def _handle_new_transmission(self, packet: scapy.packet.Packet):
         self.event_handlers.on_new_send(packet, self.state)
         self.state.high_tx_mark = packet[TCP].seq
 
     def _handle_retransmission(self, packet: scapy.packet.Packet):
+        self._enter_recovery(packet, self.state)
         self.event_handlers.on_retransmission(packet, self.state)
 
+    def _enter_recovery(self, packet: scapy.packet.Packet, state: SocketState):
+        self.state.high_rtx = packet[TCP].seq
+        self.state.recovery_point = state.high_tx_mark
+        self.state.in_recovery = True
+        self.event_handlers.on_enter_recovery(state)
+
+    def _exit_recovery(self):
+        self.event_handlers.on_exit_recovery(self.state)
+        self.state.high_rtx = 0
+        self.state.recovery_point = 0
+        self.state.in_recovery = False
+
+    def _handle_retransmission_timeout(self, packet: scapy.packet.Packet):
+        self.event_handlers.on_retransmission_timeout(packet, self.state)
+        self._exit_recovery()
+
     def _handle_send(self, packet: scapy.packet.Packet) -> None:
+        if Raw not in packet:
+            return
+
         if packet[TCP].seq > self.state.high_tx_mark:
             self._handle_new_transmission(packet)
         else:
-            self._handle_retransmission(packet)
+            if (
+                float(packet.time)
+                - float(self.state.last_sent_timestamps[packet[TCP].seq])
+                >= 0.9
+            ):
+                self._handle_retransmission_timeout(packet)
+            else:
+                self._handle_retransmission(packet)
+
+        self.state.last_sent_timestamps[packet[TCP].seq] = float(packet.time)
+        self.state.last_send_timestamp = float(packet.time)
 
     def run(self) -> None:
         for packet in self.file.packets:
+            self.state.time = float(packet.time)
             if packet[IP].dst == self.source:
                 self._handle_ack(packet)
                 continue
